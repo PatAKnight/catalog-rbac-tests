8f30055c69068ed0ce4bf70f53b808eeaeec2627
Patrick Knight <pknight@redhat.com>
2024-02-14 11:38:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# CATALOG-RBAC-TESTS
+
+Search and replace `<your-username>` with your username
+
+Edit `./catalog-entities/all.local.yaml` to select the number of users and groups that you wish to load into your backstage instance
+
+Each user file has 13 users
+
+Each group file has 13 groups
+
+Add the following to your app-config
+
+```YAML
+catalog:
+  # ... other configuration values ... #
+  locations:
+    # Local example template
+    - type: file
+      target: ../../catalog-entities/all.local.yaml
+      rules:
+        - allow: [User, Group]
+```
+
+Update the `rbac-policy.csv` to include the permissions and roles that you wish to load. Best to make a copy and then only add the permissions and roles that match the users and groups that you added above.
+
+It is setup in groups of four. So, the first four set of roles and permissions correspond to the first four user files and group files in `./catalog-entities/all.local.yaml`.
+
+Also included is `curl.py` which will allow you to make 10 groups of 15 calls to the catalog.
+
+```bash
+export TOKEN=your-token
+python curl.py
+```
---
+++ curl.py
@@ -0,0 +1,30 @@
+import requests
+import time
+import os
+
+AUTH_TOKEN = os.environ.get('TOKEN')
+
+ENDPOINT_URL = "http://localhost:7007/api/catalog/entities"
+NUM_REQUESTS = 15
+NUM_REQUESTS_TWO = 10
+total_time = 0
+
+for i in range(NUM_REQUESTS_TWO):
+    total_time_inner = 0
+    print(f"Iteration {i+1}:")
+    
+    for j in range(NUM_REQUESTS):
+        start_time_inner = time.time()
+        response = requests.get(ENDPOINT_URL, headers={"Authorization": "Bearer " + AUTH_TOKEN})
+        end_time_inner = time.time()
+        elapsed_time_inner = end_time_inner - start_time_inner
+        total_time_inner += elapsed_time_inner
+    
+    average_time_inner = total_time_inner / NUM_REQUESTS
+    print(f"Average time taken to complete 15 requests: {average_time_inner:.3f} seconds")
+    print(f"Total time it took to complete 15 requests: {total_time_inner:.3f} seconds")
+    total_time += total_time_inner
+
+average_time = total_time / NUM_REQUESTS_TWO
+print(f"Average time taken for {NUM_REQUESTS} requests over {NUM_REQUESTS_TWO} iterations: {average_time:.3f} seconds")
+print(f"Total time taken for {NUM_REQUESTS} requests of {NUM_REQUESTS_TWO} iterations: {total_time:.3f} seconds")
